9178b92f587a210b8517723f05da54438421771b
MrMisterMisterMister <22003908@student.hhs.nl>
2023-12-26 03:20:59 +0100

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -16,19 +16,19 @@ var app = builder.Build();
 app.UseForwardedHeaders();
 app.MapControllers();
 
-using var scope = app.Services.CreateScope(); // creates scope for services to setup local database and discards itself
-var services = scope.ServiceProvider;
+// using var scope = app.Services.CreateScope(); // creates scope for services to setup local database and discards itself
+// var services = scope.ServiceProvider;
 
-try
-{
-    var databaseContext = services.GetRequiredService<DatabaseContext>();
-    await databaseContext.Database.MigrateAsync(); // executes dotnet ef update on the latest migrations
-    await Seed.SeedAll(databaseContext); // inserts seed data into the database
-}
-catch (Exception e)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(e, "An error occured during migration");
-}
+// try
+// {
+//     var databaseContext = services.GetRequiredService<DatabaseContext>();
+//     await databaseContext.Database.MigrateAsync(); // executes dotnet ef update on the latest migrations
+//     await Seed.SeedAll(databaseContext); // inserts seed data into the database
+// }
+// catch (Exception e)
+// {
+//     var logger = services.GetRequiredService<ILogger<Program>>();
+//     logger.LogError(e, "An error occured during migration");
+// }
 
 app.Run();
