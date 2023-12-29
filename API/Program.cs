499e40b9b3b0cb85b49a2e92e9d77e4531649e50
Christopher Yeung <jackknife29jk@gmail.com>
2023-12-29 11:56:30 +0100

    added google services
    added google services

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Persistence.SeedData;
@@ -10,12 +12,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration); // Services should be added in the extension method 
 
+builder.Services.AddAuthentication( options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme; // Use Google authentication
+})
+.AddCookie()
+.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+{
+    options.ClientId = builder.Configuration["GoogleAuth:ClientId"]; // Retrieve ClientId from appsettings.json
+    options.ClientSecret = builder.Configuration["GoogleAuth:ClientSecret"]; // You may store ClientSecret similarly
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. HTTPS will be handled by NGINX
 app.UseForwardedHeaders();
 app.MapControllers();
 app.UseCors("CorsPolicy");
+app.UseAuthentication();
 
 using var scope = app.Services.CreateScope(); // creates scope for services to setup local database and discards itself
 var services = scope.ServiceProvider;
