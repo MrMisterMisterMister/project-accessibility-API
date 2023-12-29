6e63548f9678cb30de56cfa63c0790970d202409
Christopher Yeung <jackknife29jk@gmail.com>
2023-12-29 11:18:56 +0100

    GoogleSignIncontroller class
    GoogleSignIncontroller class

---
+++ API/Controllers/GoogleSignInController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GoogleSignInController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public GoogleSignInController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("googleClientId")]
+        public IActionResult GetGoogleClientId()
+        {
+            // Retrieve the Google Client ID from appsettings.json
+            string clientId = _configuration.GetValue<string>("GoogleAuth:ClientId");
+
+            if (string.IsNullOrEmpty(clientId))
+            {
+                return NotFound("Google Client ID not found");
+            }
+
+            return Ok(new { clientId });
+        }
+    }
+}
