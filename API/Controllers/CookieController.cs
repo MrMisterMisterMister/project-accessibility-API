0bffdbdd6688599d9a1a02a26af8b2072577606e
MrMisterMisterMister <114733626+MrMisterMisterMister@users.noreply.github.com>
2024-01-08 10:12:20 +0100

    yum yum

---
+++ API/Controllers/CookieController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CookiesController : BaseApiController
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CookiesController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet("getAllCookies")]
+        public IActionResult GetAllCookies()
+        {
+            // Access the HttpContext from the accessor
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            // Get all cookies from the Request object
+            var cookies = httpContext!.Request.Cookies;
+
+            return Ok(cookies);
+        }
+
+        [HttpPost("removeCookie")]
+        public IActionResult RemoveCookie([FromBody] string cookieName)
+        {
+            // Access the HttpContext from the accessor
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            // Remove the specified cookie
+            httpContext!.Response.Cookies.Delete(cookieName);
+
+            return Ok("Cookie removed successfully");
+        }
+    }
+}
