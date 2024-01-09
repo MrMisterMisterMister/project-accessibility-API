cec9f5003716fd12791af087c05d722ed7ebbde6
Junyi Xie <yunyi.xie@outlook.com>
2024-01-09 18:07:30 +0100

    firstName and lastName non nullable fix

--- Domain/Models/Users/PanelMember.cs
+++ Domain/Models/Users/PanelMember.cs
@@ -1,15 +1,17 @@
 using System.ComponentModel.DataAnnotations;
-using Domain;
 
-public class PanelMember : User
+namespace Domain
 {
-    [Required]
-    public int Guardian { get; set; }
-    [Required]
-    public string FirstName { get; set; }
-    [Required]
-    public string LastName { get; set; }
-    public string? Zipcode { get; set; }
-    public DateTime DateOfBirth { get; set; }
 
+    public class PanelMember : User
+    {
+        [Required]
+        public int Guardian { get; set; }
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+        public string? Zipcode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
 }
