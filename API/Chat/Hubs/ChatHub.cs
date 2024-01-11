e1576f05155e22b1fdd6c6aa73de6b7d7fcb6366
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-11 09:44:05 +0100

    somethingggg

--- API/Chat/Hubs/ChatHub.cs
+++ API/Chat/Hubs/ChatHub.cs
@@ -1,20 +1,23 @@
 ﻿using API.Chat.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Text.RegularExpressions;
 
 namespace API.Chat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task JoinChat(UserConnection conn)
-        {
-            await Clients.All
-                .SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined.");
-        }
-
+        // This method can be used to notify all users in a specific room
         public async Task JoinRoom(UserConnection conn)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
             await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined {conn.ChatRoom}.");
+        }
+
+        // Add a method to handle sending messages to a specific room
+        public async Task SendMessageToRoom(string chatRoom, string message)
+        {
+            var username = Context.User.Identity.Name; // Assuming username is available in the User Identity
+            await Clients.Group(chatRoom).SendAsync("ReceiveMessage", username, message);
         }
     }
 }
