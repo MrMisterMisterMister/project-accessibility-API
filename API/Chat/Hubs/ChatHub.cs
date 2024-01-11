13c03a1b9a980f3ce5a834c5a3e9a12d52eb1811
Christopher Yeung <jackknife29jk@gmail.com>
2024-01-11 09:49:46 +0100

    commit

--- API/Chat/Hubs/ChatHub.cs
+++ API/Chat/Hubs/ChatHub.cs
@@ -10,13 +10,13 @@ namespace API.Chat.Hubs
         public async Task JoinRoom(UserConnection conn)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
-            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined {conn.ChatRoom}.");
+            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "System", $"{conn.Username} has joined {conn.ChatRoom}.");
         }
 
         // Add a method to handle sending messages to a specific room
         public async Task SendMessageToRoom(string chatRoom, string message)
         {
-            var username = Context.User.Identity.Name; // Assuming username is available in the User Identity
+            var username = Context.User.Identity.Name; // use the userController maybe 
             await Clients.Group(chatRoom).SendAsync("ReceiveMessage", username, message);
         }
     }
