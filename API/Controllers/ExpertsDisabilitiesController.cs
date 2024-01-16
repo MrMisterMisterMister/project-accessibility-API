8168fefcd9848ab759dea681edf3534426ab6133
MrMisterMisterMister <22003908@student.hhs.nl>
2024-01-16 02:00:41 +0100

    t

---
+++ API/Controllers/ExpertsDisabilitiesController.cs
@@ -0,0 +1,22 @@
+using Application.Handlers.ExpertDisabilityHandlers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class ExpertsDisabilitiesController : BaseApiController
+    {
+        [HttpPost("add-disability/{disabilityId}")]
+        public async Task<IActionResult> AddDisabilityToExpert(int disabilityId)
+        {
+            return HandleResult(await Mediator.Send(new AddExpertDisability.Command { DisabilityId = disabilityId }));
+        }
+
+        [HttpDelete("remove-disability/{disabilityId}")]
+        public async Task<IActionResult> RemoveDisabilityFromExpert (int disabilityId)
+        {
+            return HandleResult(await Mediator.Send(new DeleteExpertDisability.Command { DisabilityId = disabilityId }));
+        }
+    }
+}
