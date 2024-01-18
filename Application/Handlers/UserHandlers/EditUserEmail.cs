020f6e8658b7ea78bbf332af93dd4dd1392bd790
Junyi Xie <yunyi.xie@outlook.com>
2024-01-18 10:44:44 +0100

    Update EditUserEmail.cs

--- Application/Handlers/UserHandlers/EditUserEmail.cs
+++ Application/Handlers/UserHandlers/EditUserEmail.cs
@@ -29,17 +29,12 @@ namespace Application.Handlers.UserHandlers
             {
                 if (string.IsNullOrEmpty(request.NewEmail))
                     return Result<Unit>.Failure("EmailIsNullOrEmpty", "Email cannot be null or empty.");
-                
+
                 if (request.NewEmail != request.ConfirmEmail)
                     return Result<Unit>.Failure("EmailsDoNotMatch", "Emails do not match.");
 
                 if (string.IsNullOrEmpty(request.Password))
                     return Result<Unit>.Failure("PasswordIsNullOrEmpty", "Password cannot be null or empty.");
-
-                var emailCheck = await _userManager.FindByEmailAsync(request.NewEmail);
-
-                if (emailCheck != null)
-                    return Result<Unit>.Failure("EmailIsTaken", "Email is in use by someone else");
 
                 var user = await _userManager.FindByEmailAsync(_userAccessor.GetEmail());
 
@@ -48,10 +43,15 @@ namespace Application.Handlers.UserHandlers
                 if (user.Email == request.NewEmail)
                     return Result<Unit>.Failure("EmailIsTheSame", "Email cannot be the same.");
 
+                var emailCheck = await _userManager.FindByEmailAsync(request.NewEmail);
+
+                if (emailCheck != null)
+                    return Result<Unit>.Failure("EmailIsTaken", "Email is in use by someone else");
+
                 var passwordCheck = await _userManager.CheckPasswordAsync(user, request.Password);
 
                 if (!passwordCheck)
-                    return Result<Unit>.Failure("IncorrectPassword", 
+                    return Result<Unit>.Failure("IncorrectPassword",
                         "Incorrect password. Please check your password and try again.");
 
                 // var token = await _userManager.GenerateChangeEmailTokenAsync(user, request.Email);
